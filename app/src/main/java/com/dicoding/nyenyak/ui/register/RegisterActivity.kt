57223db4a4b0d93d2e6d71026f21468b4226b3f1
atsalrii <21104057@ittelkom-pwt.ac.id>
2023-12-02 10:58:38 +0700

    Commit ui xml register screen

---
+++ app/src/main/java/com/dicoding/nyenyak/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.nyenyak.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.nyenyak.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}

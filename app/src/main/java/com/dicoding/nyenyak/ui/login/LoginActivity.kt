5774939f908090b3e37b75304d74d774112769d9
atsalrii <21104057@ittelkom-pwt.ac.id>
2023-12-02 10:58:58 +0700

    Commit ui xml login screen

---
+++ app/src/main/java/com/dicoding/nyenyak/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.nyenyak.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.nyenyak.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
